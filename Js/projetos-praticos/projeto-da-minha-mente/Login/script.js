976b47df49b851385f2383a416c3643acc166b7a
Daniel Oliveira <daniel33031@gmail.com>
2022-12-18 01:34:11 -0300

    deleting promise of script js of login project

--- Js/projetos-praticos/projeto-da-minha-mente/Login/script.js
+++ Js/projetos-praticos/projeto-da-minha-mente/Login/script.js
@@ -2,14 +2,14 @@
 setTimeout(() => {
     const form = document.querySelector('.form-container');
     const body = document.querySelector('body');
-    renderForm;
+    this.renderForm = false;
 
     if(form.style.opacity == 0) {
         
         form.style.opacity = 200;
         body.style.backgroundColor = 'rgb(212, 212, 212)';
         
-        return renderForm = true;   
+        return this.renderForm = true;   
 
     } else {
         
@@ -19,14 +19,22 @@ setTimeout(() => {
     
 }, 1500);
 
-this.renderForm = true;   
+   
 
 console.log(this.renderForm);
 
 class LoginValues {
+    
+
     constructor(user, password) {
         this.valueUser = user;
         this.valuePassword =  password;
+    }
+    get() {
+
+    }
+    set() {
+
     }
 
 }
@@ -48,13 +56,4 @@ btnSubmit.addEventListener('click', (e)=> {
             console.log(valuesLogin)
         }
     }
-
-    let promisseLogin = new Promise(function(myResolve, myReject){
-        myResolve(valuesLogin == true);
-        myReject(valuesLogin == false);
-    });
-    promisseLogin.then(
-        function(value) {console.log('Logou')},
-        function(error) {console.error('Não logou')}
-    )
 })
