445ce7ef2789a73031c71b3a4dd47adea72cddee
Daniel Oliveira <daniel.oliveira@seduh.df.gov.br>
2022-12-26 00:59:26 -0300

    uploading data of task miniature imagem and send files by Js

--- Js/thumbnails-com js/script.js
+++ Js/thumbnails-com js/script.js
@@ -1,3 +1,11 @@
 function show() {
-    
+    let imagem = document.getElementById("imagem").files[0];
+    //console.log(img)
+
+    let img = document.createElement('img');
+    img.src = URL.createObjectURL(imagem);
+
+    img.width = 200;
+
+    document.getElementById("area").appendChild(img);
 }
