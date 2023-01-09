c3d20fca2bf8859939bc12d7df778ce1cd8c12c3
Daniel Oliveira <daniel.oliveira@seduh.df.gov.br>
2023-01-09 15:07:10 -0300

    uploading data of tasks typescript

---
+++ Ts/trabalhando-nivel-projeto/src/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const nome = 'Bonieky';
+function teste(num1) {
+    return 'O parametro era' + num1;
+}
+teste(5);
---
+++ Ts/watchMode/src/watchMode.js
@@ -0,0 +1,5 @@
+var nome = 'Bonieky';
+function teste(num1) {
+    return 'O parametro era' + num1;
+}
+teste(5);
